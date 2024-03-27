0baca7db611fa831922e25f649140a6860e4787f
AutinC <1743551466@qq.com>
2024-03-27 11:24:25 +0800

    Site updated: 2024-03-27 11:23:55

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,5 +1,4 @@
 // 代码块语言识别
-
 $(function () {
   var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
 
@@ -14,7 +13,7 @@ $(function () {
 
     // 首字母大写
     // lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
-    
+
     $(this).siblings(".code_lang").text(lang_name);
   });
 });
